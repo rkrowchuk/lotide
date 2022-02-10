58b0107dd0b653856acea4396bf2ba25a3464c11
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-02-09 19:22:17 -0800

    function to find by key value

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+// Assert Function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  const genres = Object.keys(obj);
+  for (let genre of genres) {
+    if (obj[genre] === value) {
+      return genre;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
