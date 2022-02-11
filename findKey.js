45509e3d2fbc247f4741a058b7f35640efb90661
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-02-10 18:44:10 -0800

    function to find a key within an object

---
+++ findKey.js
@@ -0,0 +1,37 @@
+// Assertion function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// findKey function
+const findKey = function(obj, callback) {
+  const items = Object.keys(obj);
+  for (let item of items) {
+    if (callback(obj[item])) {
+      return item;
+    }
+  }
+};
+
+// tests
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"); // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 3 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 1 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), undefined); // => "noma"
