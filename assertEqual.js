8613fa0f3d3b1d3c2ad29d37f3bf3842933418d8
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-02-14 07:38:45 -0800

    move tests to test file

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,11 @@
-// FUNCTION IMPLEMENTATION
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅ ✅ ✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🛑 🛑 🛑 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-// TEST CODE
-assertEqual("lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("red", "red");
-assertEqual(6, 9);
+
+module.exports = assertEqual;
