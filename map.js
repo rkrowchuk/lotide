8cda0004c355034a458eaef9b1cb8296b2050394
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-02-14 07:41:28 -0800

    formatting

--- map.js
+++ map.js
@@ -45,7 +45,7 @@ const results1 = map(words, word => word[0]);
 console.log(results1);
 const results2 = map(words2, word => word[0]);
 console.log(results2);
-const results3 = map(words3, word => word[0]);
+const results3 = map(words3, word => word[0]); 
 console.log(results3);
 
 
